fd736e8b75c297b9ae86f573b7c76e89bb7de38b
Denny Wahyu <janexmgd@gmail.com>
2022-05-14 20:21:00 +0700

    first commit

--- src/pages/airline/index.jsx
+++ src/pages/airline/index.jsx
@@ -2,11 +2,20 @@ import React, { useEffect } from 'react';
 import { APP_NAME } from '../../helpers/env';
 import ContentHeader from '../../components/content-header';
 import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import ContentLoader from 'react-content-loader';
+
+import { getAllAirline } from '../../redux/actions/allAirlane';
 
 import $ from 'jquery';
-import img from '../../assets/images/garuda.png';
+// import img from '../../assets/images/garuda.png';
 
 const index = () => {
+  const dispatch = useDispatch();
+
+  const allAirlane = useSelector((state) => {
+    return state.allAirlineReducer;
+  });
   useEffect(() => {
     document.title = `${APP_NAME} - Management Airline`;
     $(document).ready(function () {
@@ -14,6 +23,8 @@ const index = () => {
         $('#example1').DataTable();
       }, 1000);
     });
+
+    dispatch(getAllAirline());
   }, []);
 
   return (
@@ -33,18 +44,62 @@ const index = () => {
                   <Link to="/airline/add" className="btn btn-primary mb-3">
                     <i className="fa fa-plus"></i> Add Airline
                   </Link>
-                  <table id="example1" className="table table-bordered table-striped table-hover">
-                    <thead>
-                      <tr>
-                        <th>No</th>
-                        <th>Name</th>
-                        <th>Image</th>
-                        <th>Status</th>
-                        <th>Option</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
+                  {allAirlane.isLoading ? (
+                    <ContentLoader />
+                  ) : allAirlane.isError ? (
+                    <div>Error</div>
+                  ) : (
+                    <table id="example1" className="table table-bordered table-striped table-hover">
+                      <thead>
+                        <tr>
+                          <th>No</th>
+                          <th>Name</th>
+                          <th>Image</th>
+                          <th>Status</th>
+                          <th>Option</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {allAirlane.data.data.map((item, i) => (
+                          <tr key={i}>
+                            <td>{i + 1}</td>
+                            <td>{item.name}</td>
+                            <td>
+                              <img
+                                src={`${process.env.REACT_APP_PROD}uploads/airlines/${item.image}`}
+                                alt={`${item.name}`}
+                                onError={(e) => {
+                                  e.target.src = `${process.env.REACT_APP_PROD}uploads/airlines/airlines-default.png`;
+                                }}
+                                style={{ width: '60px' }}
+                              />
+                            </td>
+                            <td>
+                              {item.is_active == 1 ? (
+                                <span className="badge badge-pill badge-success">Active</span>
+                              ) : (
+                                <span className="badge badge-pill badge-danger">Non Active</span>
+                              )}
+                            </td>
+                            <td className="text-center">
+                              <a
+                                className="btn btn-primary btn-sm"
+                                href={`airline/view/${item.id}`}>
+                                <i className="fas fa-eye"></i>
+                                View
+                              </a>
+                              <a className="btn btn-info btn-sm ml-2" href="#">
+                                <i className="fas fa-pencil-alt"></i>
+                                Edit
+                              </a>
+                              <a className="btn btn-danger btn-sm ml-2" href="#">
+                                <i className="fas fa-trash"></i>
+                                Delete
+                              </a>
+                            </td>
+                          </tr>
+                        ))}
+                        {/* <tr>
                         <td>1</td>
                         <td>Garuda Indonesia</td>
                         <td>
@@ -67,18 +122,19 @@ const index = () => {
                             Delete
                           </a>
                         </td>
-                      </tr>
-                    </tbody>
-                    <tfoot>
-                      <tr>
-                        <th>No</th>
-                        <th>Name</th>
-                        <th>Image</th>
-                        <th>Status</th>
-                        <th>Option</th>
-                      </tr>
-                    </tfoot>
-                  </table>
+                      </tr> */}
+                      </tbody>
+                      <tfoot>
+                        <tr>
+                          <th>No</th>
+                          <th>Name</th>
+                          <th>Image</th>
+                          <th>Status</th>
+                          <th>Option</th>
+                        </tr>
+                      </tfoot>
+                    </table>
+                  )}
                 </div>
                 {/* /.card-body */}
               </div>
--- src/pages/flight/index.jsx
+++ src/pages/flight/index.jsx
@@ -1,11 +1,21 @@
 import React, { useEffect } from 'react';
 import { APP_NAME } from '../../helpers/env';
+import { useSelector, useDispatch } from 'react-redux';
 import ContentHeader from '../../components/content-header';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+
+import { getAllFlight } from '../../redux/actions/allFlight';
 
 import $ from 'jquery';
+import ContentLoader from 'react-content-loader';
 
 const index = () => {
+  const dispatch = useDispatch();
+
+  const allFlight = useSelector((state) => {
+    return state.allFlightReducer;
+  });
   useEffect(() => {
     document.title = `${APP_NAME} - Management Ticket`;
     $(document).ready(function () {
@@ -13,6 +23,8 @@ const index = () => {
         $('#example1').DataTable();
       }, 1000);
     });
+    dispatch(getAllFlight());
+    console.log(allFlight);
   }, []);
 
   return (
@@ -32,53 +44,65 @@ const index = () => {
                   <Link to="/flight/add" className="btn btn-primary mb-3">
                     <i className="fa fa-plus"></i> Add Flight
                   </Link>
-                  <table id="example1" className="table table-bordered table-striped table-hover">
-                    <thead>
-                      <tr>
-                        <th>No</th>
-                        <th>Name</th>
-                        <th>Departure Date</th>
-                        <th>PIC</th>
-                        <th>Status</th>
-                        <th>Option</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td>1</td>
-                        <td>Garuda Indonesia</td>
-                        <td>Monday, 13 May 2022</td>
-                        <td>John Wick</td>
-                        <td>
-                          <span className="badge badge-pill badge-primary">Active</span>
-                        </td>
-                        <td className="text-center">
-                          <a className="btn btn-primary btn-sm" href="#">
-                            <i className="fas fa-eye"></i>
-                            View
-                          </a>
-                          <a className="btn btn-info btn-sm ml-2" href="#">
-                            <i className="fas fa-pencil-alt"></i>
-                            Edit
-                          </a>
-                          <a className="btn btn-danger btn-sm ml-2" href="#">
-                            <i className="fas fa-trash"></i>
-                            Delete
-                          </a>
-                        </td>
-                      </tr>
-                    </tbody>
-                    <tfoot>
-                      <tr>
-                        <th>No</th>
-                        <th>Name</th>
-                        <th>Departure Date</th>
-                        <th>PIC</th>
-                        <th>Status</th>
-                        <th>Option</th>
-                      </tr>
-                    </tfoot>
-                  </table>
+                  {allFlight.isLoading ? (
+                    <ContentLoader />
+                  ) : allFlight.isError ? (
+                    <div>Error</div>
+                  ) : (
+                    <table id="example1" className="table table-bordered table-striped table-hover">
+                      <thead>
+                        <tr>
+                          <th>No</th>
+                          <th>Name</th>
+                          <th>Departure Date</th>
+                          <th>PIC</th>
+                          <th>Status</th>
+                          <th>Option</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {allFlight.data.data.map((item, i) => (
+                          <tr key={i}>
+                            <td>{i + 1}</td>
+                            <td>{item.airlinesname}</td>
+                            <td>{moment(item.departuredate).format('dddd,DD-MM-YYYY')}</td>
+                            <td>{item.picname}</td>
+                            <td>
+                              {item.is_active == 1 ? (
+                                <span className="badge badge-pill badge-success">Active</span>
+                              ) : (
+                                <span className="badge badge-pill badge-danger">Non Active</span>
+                              )}
+                            </td>
+                            <td className="text-center">
+                              <a className="btn btn-primary btn-sm" href="#">
+                                <i className="fas fa-eye"></i>
+                                View
+                              </a>
+                              <a className="btn btn-info btn-sm ml-2" href="#">
+                                <i className="fas fa-pencil-alt"></i>
+                                Edit
+                              </a>
+                              <a className="btn btn-danger btn-sm ml-2" href="#">
+                                <i className="fas fa-trash"></i>
+                                Delete
+                              </a>
+                            </td>
+                          </tr>
+                        ))}
+                      </tbody>
+                      <tfoot>
+                        <tr>
+                          <th>No</th>
+                          <th>Name</th>
+                          <th>Departure Date</th>
+                          <th>PIC</th>
+                          <th>Status</th>
+                          <th>Option</th>
+                        </tr>
+                      </tfoot>
+                    </table>
+                  )}
                 </div>
                 {/* /.card-body */}
               </div>
